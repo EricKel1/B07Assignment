4164f96fefb0a03f4d9a22cd7a943e0ce2c62743
Eric <eric.kel@outlook.com>
2025-11-13 14:37:09 -0500

    test(log): add unit tests for RescueInhalerLog and RescueInhalerRepository

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
+    testImplementation("org.mockito:mockito-core:5.5.0")
+    testImplementation("org.mockito:mockito-inline:5.2.0")
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation(platform(libs.firebase.bom))
